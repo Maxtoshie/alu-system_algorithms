7dcc0c0f74d9f3de49b9b386378fa325618a5b78
Maxtoshie <maaxwelokoth@gmail.com>
2026-01-29 19:42:07 +0300

    a function that creates a generic Binary Tree node

--- huffman_coding/heap/binary_tree_node.c
+++ huffman_coding/heap/binary_tree_node.c
@@ -1,18 +1,28 @@
 #include <stdlib.h>
-#include <string.h>
-
 #include "heap.h"
 
 /**
- * binary_tree_node - creates a generic binary node with no children
- * @parent: parent node to new node
- * @data: void pointer to generic key value for new node
- * Return: pointer to new node or NULL on failure
+ * binary_tree_node - creates a generic binary tree node
+ * @parent: Pointer to the parent node
+ * @data: Data to store in the node
+ *
+ * Return: Pointer to the created node, or NULL on failure
  */
 binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
-{C99(
-	binary_tree_node_t src = (binary_tree_node_t){data, NULL, NULL, parent};
-	binary_tree_node_t *dst = calloc(1, sizeof(*dst));
-	if (!dst || !data) { free(dst); return (NULL);}
-	return (memcpy(dst, &src, sizeof(src)));
-);}
+{
+	binary_tree_node_t *node;
+
+	if (data == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(binary_tree_node_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+
+	return (node);
+}
--- huffman_coding/heap/heap_insert.c
+++ huffman_coding/heap/heap_insert.c
@@ -1,5 +1,4 @@
 #include "heap.h"
-#include <stdio.h>
 
 /**
  * heapify - restores heap property from leaf node of subtree
@@ -9,39 +8,50 @@
  */
 static node_t *heapify(node_t *node, int (*data_cmp)(void *, void *))
 {
-	if (!node || !data_cmp) return (NULL);
+	if (!node || !data_cmp)
+		return (NULL);
+
 	while (node->parent && data_cmp(node->data, node->parent->data) < 0)
 	{
 		DATASWAP(node, node->parent);
 		node = node->parent;
 	}
+
 	return (node);
 }
 
 /**
  * heap_insert - inserts a value in a binary heap
- * *		 (uses static reference array)
+ *                  (uses static reference array)
  * @heap: pointer to heap_t where we'll insert node
  * @data: generic pointer to data for new node
  * Return: node_t pointer to the newly inserted node
  */
 node_t *heap_insert(heap_t *heap, void *data)
-{C99(
-	if (!heap || !data) return (NULL);
+{
 	static node_t *harray[32];
-	if (!heap->size)
+	node_t *new, *parent;
+
+	if (!heap || !data)
+		return (NULL);
+
+	if (heap->size == 0)
 	{
 		harray[heap->size++] = binary_tree_node(NULL, data);
-DBG(PRINT_NODE_ARRAY((const node_t **)&harray, heap->size));
-DBG(printf("heap->size now %ld\n\n", heap->size));
-		return (heap->root = harray[0]);
+		heap->root = harray[0];
+		return (heap->root);
 	}
-	node_t *parent = harray[(heap->size - 1) / 2];
+
+	/* find parent of new node */
+	parent = harray[(heap->size - 1) / 2];
+
+	/* insert left or right */
 	harray[heap->size++] = (!parent->left)
-			? (parent->left = binary_tree_node(parent, data))
-			: (parent->right = binary_tree_node(parent, data));
-	node_t *new = (heapify(harray[heap->size - 1], heap->data_cmp));
-DBG(PRINT_NODE_ARRAY((const node_t **)&harray, heap->size));
-DBG(printf("heap->size now %ld\n\n", heap->size));
+		? (parent->left = binary_tree_node(parent, data))
+		: (parent->right = binary_tree_node(parent, data));
+
+	/* restore heap property */
+	new = heapify(harray[heap->size - 1], heap->data_cmp);
+
 	return (new);
-);}
+}
