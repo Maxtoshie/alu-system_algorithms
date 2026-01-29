2b1b8ad3bcbcd4d352231f0bed6705976722a000
Maxtoshie <maaxwelokoth@gmail.com>
2026-01-29 19:57:35 +0300

    Fixes memory issues

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -1,21 +1,40 @@
 #include "huffman.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * huffman_tree - description
- * @data: data
- * @freq: freq
- * @size: size
- * Return: int
+ * huffman_tree - builds a Huffman tree from given data and frequencies
+ * @data: array of characters
+ * @freq: array of corresponding frequencies
+ * @size: size of both arrays
+ *
+ * Return: pointer to root node of Huffman tree, or NULL on failure
  */
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
-{C99(
-	heap_t *q = huffman_priority_queue(data, freq, size);
-	while (q && q->size > 1)
+{
+	heap_t *q;
+	node_t *left, *right, *parent;
+
+	if (!data || !freq || size == 0)
+		return (NULL);
+
+	q = huffman_priority_queue(data, freq, size);
+	if (!q)
+		return (NULL);
+
+	/* Extract two smallest nodes and combine until only one node remains */
+	while (q->size > 1)
 		huffman_extract_and_insert(q);
-	node_t *huff = q->root->data;
+
+	/* Get the root of Huffman tree */
+	if (!q->root)
+	{
+		free(q);
+		return (NULL);
+	}
+
+	parent = q->root->data;
 	free(q->root);
 	free(q);
-	return (huff);
-);}
+
+	return (parent);
+}
