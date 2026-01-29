75a31b3b77fe3c7345f3b2e40c8a94172b6d009f
Maxtoshie <maaxwelokoth@gmail.com>
2026-01-29 19:47:23 +0300

    Fixes c99

--- huffman_coding/heap/heap_extract.c
+++ huffman_coding/heap/heap_extract.c
@@ -1,15 +1,11 @@
-#include <stdlib.h>
 #include "heap.h"
-#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * heapify - restores heap property from root node of subtree
- * @root: hode_t root root of heap tree
- * @data_cmp: comparison function over generic data pointers
- */
+/* Helper: restore heap property from root downwards */
 static void heapify(node_t *root, int (*data_cmp)(void *, void *))
 {
-	if (!root || !root->left || !data_cmp) return;
+	if (!root || !root->left || !data_cmp)
+		return;
 
 	if (root->right && data_cmp(root->left->data, root->right->data) > 0)
 	{
@@ -25,35 +21,41 @@ static void heapify(node_t *root, int (*data_cmp)(void *, void *))
 	}
 }
 
-/**
- * heap_to_array - it does this while calculating heap size
- * @array: node_t buffer where we are storing our heap elements
- * @root: root node_t of our heap subtree
- * @i: size_t index of subtree's @root node
- * Return: size_t size of heap. the same as the length of @array
- */
+/* Helper: convert heap tree to array (optional, can suppress unused warnings) */
 static size_t heap_to_array(node_t **array, node_t *root, size_t i)
 {
-	return (!root || !array || !(array[i] = root)
-		? 0 : 1 + heap_to_array(array, root->left, 2 * i + 1)
-			+ heap_to_array(array, root->right, 2 * i + 2));
+	if (!root || !array)
+		return 0;
+
+	array[i] = root;
+	return 1 + heap_to_array(array, root->left, 2 * i + 1)
+		+ heap_to_array(array, root->right, 2 * i + 2);
 }
 
 /**
  * heap_extract - extracts the root value of a Min Binary Heap
  * @heap: heap_t pointer
- * Return: generic pointer to data that was stored in heap's root
+ * Return: generic pointer to data that was stored in heap's root, or NULL
  */
 void *heap_extract(heap_t *heap)
-{C99(
-	node_t *a[heap->size], *last;
-	void *data = heap->root->data;
+{
+	node_t *a[32], *last;
+	void *data;
 
-	if (!heap || heap->size != heap_to_array((node_t **)&a, heap->root, 0))
-		return (NULL);
+	if (!heap || !heap->root)
+		return NULL;
 
-	if (heap->root == (last = a[heap->size - 1]))
+	/* convert heap to array */
+	if (heap->size != heap_to_array(a, heap->root, 0))
+		return NULL;
+
+	data = heap->root->data;
+	last = a[heap->size - 1];
+
+	if (heap->root == last)
+	{
 		heap->root = NULL;
+	}
 	else
 	{
 		heap->root->data = last->data;
@@ -61,11 +63,12 @@ void *heap_extract(heap_t *heap)
 			last->parent->left = NULL;
 		else
 			last->parent->right = NULL;
+
 		heapify(heap->root, heap->data_cmp);
 	}
+
 	free(last);
-	--heap->size;
-DBG(PRINT_NODE_ARRAY((const node_t **)&a, heap->size));
-DBG(printf("heap->size now %ld\n\n", heap->size));
-	return (data);
-);}
+	heap->size--;
+
+	return data;
+}
